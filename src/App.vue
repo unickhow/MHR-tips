f206d61b402dc5db7426c6a76f4a56e789fe6f8d
unick_how <unick.zhow@gmail.com>
2021-05-17 02:18:50 +0800

    💡 feat: migrate to vue3 from codepen

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="wrapper" v-cloak @click="nextTip" @keyup.left="substractIndex" @keyup.right="addIndex" tabindex="0">
+    <div class="container">
+      <div class="content">
+        <h1>{{ currentTip.title }}</h1>
+        <p v-replace-button="currentTip.content"></p>
+        <small>◀︎ {{ tipIndex + 1 }}/{{ tips.length }} ►</small>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref, computed, reactive } from 'vue'
+  import tips from './assets/tips.json'
+  import buttons from './assets/buttons.json'
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+  // data
+  let tipIndex = ref(0)
+  const newTips = reactive(tips)
+  const currentTip = computed(() => newTips[tipIndex.value])
+  // 
+
+  // manipulate tip index
+  const nextTip = () => {
+    addIndex()
+  }
+	const addIndex = () => {
+		tipIndex.value = tipIndex.value < tips.length - 1 ? tipIndex.value + 1 : 0
+  }
+	const substractIndex = () => {
+		tipIndex.value = tipIndex.value < 1 ? tips.length - 1 : tipIndex.value - 1
+  }
+  // 
+
+  // pre-load images
+  // TODO: store images to local
+  const preloadImage = () => {
+    const bgImg = new Image()
+    bgImg.src = 'https://news.destructoid.com/wp-content/uploads/2021/04/MonsterHunterRiseLoadingScreenArt2.png'
+    buttons.map((button) => {
+      const img = new Image()
+      img.src = button.img
+    })
+  }
+  preloadImage()
+  // 
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,33 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style/main.scss'
+import buttons from './assets/buttons.json'
 
-createApp(App).mount('#app')
+const Instance = createApp(App)
+
+// TODO: find local directive for vue3
+const replaceButtonImage = (el, binding) => {
+  const elContent = binding.value;
+  const matchedButtons = elContent.match(
+    new RegExp(buttons.map((b) => b.key).join("|"), "g")
+  );
+  const matchedButtonsFiltered = [...new Set(matchedButtons)];
+
+  let newContext = matchedButtonsFiltered.reduce((acc, cur) => {
+    const img = buttons.find((btn) => btn.key === cur)?.img;
+    return acc.replaceAll(cur, `<img src='${img}' role='button-img'>`);
+  }, elContent);
+
+  el.innerHTML = newContext;
+}
+
+Instance.directive('replace-button', {
+  mounted(el, binding) {
+    replaceButtonImage(el, binding)
+  },
+  beforeUpdate(el, binding) {
+    replaceButtonImage(el, binding)
+  }
+})
+
+Instance.mount('#app')
